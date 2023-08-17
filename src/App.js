d04814241fa8e0636783760a150d3aa7c652042e
Jo'rabek <jorabekbaxtiyorov230@gmail.com>
2023-08-17 10:38:53 +0500

    finished

---
+++ src/App.js
@@ -0,0 +1,30 @@
+ import React, {useState} from 'react'
+import './App.css';
+
+function App() {
+  let a=0
+  const [state , setState] = useState(0)
+
+  function inc(){
+    setState(state+1)
+  }
+
+  function dec(){
+    setState(state-1)
+  }
+
+  function reset(){
+    setState(0)
+  }
+
+  return (
+    <div className="App">
+      <h1 style={{color:'red', fontSize:'50px'}}>{state}</h1>
+      <button onClick={inc} style={{fonSize:'25px' , margin:'0 15px'}}>+</button>
+      <button onClick={dec} style={{fonSize:'25px' , margin:'0 15px'}}>-</button>
+      <button onClick={reset} style={{fonSize:'25px' , margin:'0 15px'}}>reset</button>
+    </div>
+  );
+}
+
+export default App;
